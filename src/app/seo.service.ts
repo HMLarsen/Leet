8ac4c209907d1724f39fbd958176ae45e2e7a87d
Hugo <hugomarcel91@gmail.com>
2022-01-05 13:34:43 -0300

    metatags

--- src/app/seo.service.ts
+++ src/app/seo.service.ts
@@ -14,18 +14,18 @@ export class SEOService {
 	updateTitle(title: string, prefix = true) {
 		const titleUpdated = prefix ? 'Leet - ' : '' + title;
 		this.title.setTitle(titleUpdated);
-		this.meta.updateTag({ name: 'og:title', content: titleUpdated });
+		this.meta.updateTag({ property: 'og:title', content: titleUpdated });
 		this.meta.updateTag({ name: 'twitter:title', content: titleUpdated });
 	}
 
 	updateDescription(desc: string) {
 		this.meta.updateTag({ name: 'description', content: desc });
-		this.meta.updateTag({ name: 'og:description', content: desc });
+		this.meta.updateTag({ property: 'og:description', content: desc });
 		this.meta.updateTag({ name: 'twitter:description', content: desc });
 	}
 
 	updateImage(imageUrl: string) {
-		this.meta.updateTag({ name: 'og:image', content: imageUrl });
+		this.meta.updateTag({ property: 'og:image', content: imageUrl });
 		this.meta.updateTag({ name: 'twitter:image', content: imageUrl });
 	}
 
